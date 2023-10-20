f3c9a8a83a8019d79e9661aec00749d7c3fa683b
João Vitor <jvitordiass@outlook.com.br>
2023-10-19 22:21:34 -0300

    feat: add reusable stales

---
+++ src/shared/styles/index.ts
@@ -0,0 +1,23 @@
+import { SxProps, Theme } from '@mui/material'
+
+export const Title: SxProps<Theme> = {
+  '&::after': {
+    content: '""',
+    width: 60,
+    height: 8,
+    display: 'block',
+    backgroundColor: '#42a5f5'
+  }
+}
+
+export const FlexRow: SxProps<Theme> = {
+  display: 'flex',
+  alignItems: 'center',
+  gap: 1
+}
+
+export const FlexColumn: SxProps<Theme> = {
+  display: 'flex',
+  flexDirection: 'column',
+  justifyContent: 'space-between'
+}
