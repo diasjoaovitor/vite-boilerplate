3155f566954fec9c37ae69f1f6d38d5bb776c93b
João Vitor <jvitordiass@outlook.com.br>
2023-10-19 22:18:37 -0300

    fix: use yarn to run prettier

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,6 +1,6 @@
 module.exports = {
   '*.{js,jsx,ts,tsx}': filenames => [
-    `prettier --write ${filenames.join(' ')}`,
+    `yarn prettier --write ${filenames.join(' ')}`,
     `yarn eslint --fix --ext .ts,.tsx .`,
     `yarn test -- --findRelatedTests ${filenames.join(' ')} --passWithNoTests`
   ]
