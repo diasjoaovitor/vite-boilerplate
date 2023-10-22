108d2ed544385c16682d7a4f6c982dbf9cc8fc6c
João Vitor <jvitordiass@outlook.com.br>
2023-10-21 22:37:18 -0300

    test: Auth

---
+++ src/shared/components/Auth/auth.test.tsx
@@ -0,0 +1,46 @@
+import { fireEvent, render, screen } from '@testing-library/react'
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import { Auth } from '.'
+
+const mockedHandleSubmit = jest.fn((e) => e.preventDefault())
+
+function memoryRouter(handleSubmit: (e: React.FormEvent) => Promise<void>) {
+  const router = createMemoryRouter(
+    [
+      {
+        path: '/',
+        element: (
+          <Auth
+            inputs={[]}
+            redirect={{ text: 'Navigate', to: '/page2' }}
+            title="Auth"
+            handleSubmit={handleSubmit}
+          />
+        )
+      },
+      {
+        path: '/page2',
+        element: <></>
+      }
+    ],
+    {
+      initialEntries: ['/']
+    }
+  )
+  render(<RouterProvider router={router} />)
+  return router
+}
+
+describe('<Auth />', () => {
+  it('should submit the form', () => {
+    memoryRouter(mockedHandleSubmit)
+    fireEvent.click(screen.getByRole('button', { name: /Auth/i }))
+    expect(mockedHandleSubmit).toHaveBeenCalled()
+  })
+
+  it('should navigate to page 2', () => {
+    const router = memoryRouter(mockedHandleSubmit)
+    fireEvent.click(screen.getByRole('link', { name: /Navigate/i }))
+    expect(router.state.location.pathname).toBe('/page2')
+  })
+})
