ca08df243fb6fb0ebf6a81439dc873d3710598e9
João Vitor <jvitordiass@outlook.com.br>
2023-10-21 22:52:38 -0300

    refactor: add memory router because navigation was implemented in Logout

--- src/shared/components/Logout/logout.test.tsx
+++ src/shared/components/Logout/logout.test.tsx
@@ -1,9 +1,25 @@
 import { render, screen } from '@testing-library/react'
+import { RouterProvider, createMemoryRouter } from 'react-router-dom'
 import { Logout } from '.'
+
+function Page() {
+  const router = createMemoryRouter(
+    [
+      {
+        path: '/',
+        element: <Logout />
+      }
+    ],
+    {
+      initialEntries: ['/']
+    }
+  )
+  return <RouterProvider router={router} />
+}
 
 describe('<Logout />', () => {
   it('should render the button', () => {
-    render(<Logout />)
+    render(<Page />)
     expect(screen.getByRole('button', { name: /Logout/i })).toBeInTheDocument()
   })
 })
