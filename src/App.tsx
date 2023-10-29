1616ce8858247688c7af4764dc0de5ef109106d9
João Vitor <jvitordiass@outlook.com.br>
2023-10-29 18:49:29 -0300

    feat: add react query and auth context

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 import { Router } from './Router'
-import { ThemeProvider } from './shared/contexts'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { AuthProvider, ThemeProvider } from './shared/contexts'
+
+const queryClient = new QueryClient()
 
 function App() {
   return (
     <ThemeProvider>
-      <Router />
+      <QueryClientProvider client={queryClient}>
+        <AuthProvider>
+          <Router />
+        </AuthProvider>
+      </QueryClientProvider>
     </ThemeProvider>
   )
 }
