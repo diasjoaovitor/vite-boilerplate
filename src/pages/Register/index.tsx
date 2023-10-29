d4c80a3c63a7e5f0f55d73903cbbab00d61529ef
João Vitor <jvitordiass@outlook.com.br>
2023-10-29 18:51:29 -0300

    feat: Register

--- src/pages/Register/index.tsx
+++ src/pages/Register/index.tsx
@@ -1,14 +1,16 @@
 import { Auth } from '@/shared/components'
+import { register } from '@/shared/firebase'
+import { useAuth } from '@/shared/hooks'
 import { inputs } from './inputs'
 
 export function Register() {
-  const handleSubmit = async () => console.log('register')
+  const props = useAuth(register)
   return (
     <Auth
       title="Registrar"
       inputs={inputs}
       redirect={{ to: '/entrar', text: 'Já tenho uma conta!' }}
-      handleSubmit={handleSubmit}
+      {...props}
     />
   )
 }
