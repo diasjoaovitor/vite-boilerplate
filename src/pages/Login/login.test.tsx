5ecdc0d37e1ceb2da9203f9c48b6441e9f463df1
João Vitor <jvitordiass@outlook.com.br>
2023-10-29 18:50:54 -0300

    test: Login

--- src/pages/Login/login.test.tsx
+++ src/pages/Login/login.test.tsx
@@ -1,25 +1,57 @@
-import { render, screen } from '@testing-library/react'
+import { fireEvent, render, screen, waitFor } from '@testing-library/react'
 import { RouterProvider, createMemoryRouter } from 'react-router-dom'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { login } from '@/shared/firebase'
 import { Login } from '.'
 
-function Page() {
+jest.mock('../../shared/firebase')
+
+const mockedLogin = login as jest.Mock
+
+const client = new QueryClient()
+
+function memoryRouter() {
   const router = createMemoryRouter(
     [
       {
-        path: '/',
+        path: '/entrar',
         element: <Login />
+      },
+      {
+        path: '/',
+        element: <></>
       }
     ],
     {
-      initialEntries: ['/']
+      initialEntries: ['/entrar']
     }
   )
-  return <RouterProvider router={router} />
+  render(
+    <QueryClientProvider client={client}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  )
+  return router
 }
 
 describe('<Login />', () => {
-  it('should render the heading', () => {
-    render(<Page />)
-    expect(screen.getByRole('heading', { name: /Entrar/i })).toBeInTheDocument()
+  it('should login correctly', async () => {
+    mockedLogin.mockImplementation(() => Promise.resolve())
+    const router = memoryRouter()
+    fireEvent.submit(screen.getByRole('form'))
+    await waitFor(() => {
+      expect(router.state.location.pathname).toBe('/')
+    })
+  })
+
+  it('should render error message', async () => {
+    mockedLogin.mockImplementation(() => {
+      throw { code: 'auth/invalid-login-credentials' }
+    })
+    memoryRouter()
+    fireEvent.submit(screen.getByRole('form'))
+    await waitFor(() => {
+      expect(screen.getByText('Email ou senha inválidos!')).toBeInTheDocument()
+    })
   })
 })
