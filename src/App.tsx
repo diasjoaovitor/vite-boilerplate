c79bbac89f29f25b72b5e4d31ed7ae19f4927d98
João Vitor <jvitordiass@outlook.com.br>
2024-02-25 19:20:45 -0300

    chore: configure environment variables

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
 import { Main } from '@/components'
 
+const key = process.env.VITE_API_KEY
+
 export const App = () => {
   return (
     <>
       <div>App</div>
+      <div>{key}</div>
       <Main />
     </>
   )
--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,20 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, 'src/shared')
+export default defineConfig((props) => {
+  const env = loadEnv(props.mode, process.cwd(), 'VITE')
+  const envWithProcessPrefix = {
+    'process.env': `${JSON.stringify(env)}`
+  }
+  return {
+    plugins: [react()],
+    define: envWithProcessPrefix,
+    resolve: {
+      alias: {
+        '@': path.resolve(__dirname, 'src/shared')
+      }
     }
   }
 })
