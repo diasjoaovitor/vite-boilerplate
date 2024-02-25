1154ef7e64d6ec3759b60a00154d2e8a1ecc9861
João Vitor <jvitordiass@outlook.com.br>
2024-02-25 19:01:38 -0300

    chore: configure alias

--- jest.config.ts
+++ jest.config.ts
@@ -2,9 +2,12 @@ export default {
   testEnvironment: 'jsdom',
   testPathIgnorePatterns: ['/node_modules/'],
   collectCoverage: true,
-  collectCoverageFrom: ['src/**/*.ts(x)?'],
+  collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/index.ts'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
   modulePaths: ['<rootDir>/src/'],
+  moduleNameMapper: {
+    '^@/components(.*)$': '<rootDir>/src/shared/components$1'
+  },
   transform: {
     '^.+\\.(js|jsx|ts|tsx)$': [
       '@swc/jest',
--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Main } from './shared/components/Main'
+import { Main } from '@/components'
 
 export const App = () => {
   return (
