d3808667f8c6d13f71c804376bfbb9347ed1c3ae
João Vitor <jvitordiass@outlook.com>
2024-12-04 15:46:15 -0300

    chore: add `append` option to `plop` action for adding the new generated component to the index file

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,3 +1,6 @@
+import fs from 'fs'
+import path from 'path'
+
 export default (plop) => {
   plop.setGenerator('component', {
     description: 'Create a component',
@@ -18,6 +21,23 @@ export default (plop) => {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/{{kebabCase name}}.test.tsx',
         templateFile: 'templates/test.tsx.hbs'
+      },
+      {
+        type: 'append',
+        path: '../src/components/index.ts',
+        template: "export * from './{{pascalCase name}}'"
+      },
+      () => {
+        const __dirname = path.dirname(new URL(import.meta.url).pathname)
+        const indexPath = path.resolve(__dirname, '../src/components/index.ts')
+        const content = fs.readFileSync(indexPath, 'utf-8')
+        const lines = content.split('\n')
+        const updatedContent =
+          lines
+            .filter((line) => line.trim() !== '')
+            .sort()
+            .join('\n') + '\n'
+        fs.writeFileSync(indexPath, updatedContent)
       }
     ]
   })
