2704d8f2822d5d13acf82c9237788006e27debb7
João Vitor <jvitordiass@outlook.com>
2024-12-04 15:42:22 -0300

    chore: update dependencies to latest versions
      add new eslint config settings for your version 9
      add new vite config settings for your version 6

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,3 @@
-/// <reference types="vitest" />
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 import path from 'path'
@@ -7,10 +5,6 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  test: {
-    environment: 'jsdom',
-    setupFiles: ['./vitest.setup.ts']
-  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
