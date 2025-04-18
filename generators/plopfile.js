7bd76db53d0b472f0f8a7efa414f0ae01ab9d3c5
João Vitor <jvitordiass@outlook.com>
2025-04-18 09:16:18 -0300

    🦄 refactor: update component generator
    - replace old component template with a new index.tsx template
    - add logic to create index.ts if it doesn't exist
    - ensure test template imports are correctly structured

--- generators/plopfile.js
+++ generators/plopfile.js
@@ -1,6 +1,9 @@
 import fs from 'fs'
 import path from 'path'
 
+const __dirname = path.dirname(new URL(import.meta.url).pathname)
+const indexPath = path.resolve(__dirname, '../src/components/index.ts')
+
 export default (plop) => {
   plop.setGenerator('component', {
     description: 'Create a component',
@@ -15,12 +18,19 @@ export default (plop) => {
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/index.tsx',
-        templateFile: 'templates/Component.tsx.hbs'
+        templateFile: 'templates/index.tsx.hbs'
       },
       {
         type: 'add',
         path: '../src/components/{{pascalCase name}}/{{kebabCase name}}.test.tsx',
         templateFile: 'templates/test.tsx.hbs'
+      },
+      () => {
+        if (!fs.existsSync(indexPath)) {
+          const dir = path.dirname(indexPath)
+          fs.mkdirSync(dir, { recursive: true })
+          fs.writeFileSync(indexPath, '')
+        }
       },
       {
         type: 'append',
@@ -28,8 +38,6 @@ export default (plop) => {
         template: "export * from './{{pascalCase name}}'"
       },
       () => {
-        const __dirname = path.dirname(new URL(import.meta.url).pathname)
-        const indexPath = path.resolve(__dirname, '../src/components/index.ts')
         const content = fs.readFileSync(indexPath, 'utf-8')
         const lines = content.split('\n')
         const updatedContent =
